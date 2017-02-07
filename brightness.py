4851a6eb6e87b392f7db876729f6c53d79f7da50
Eriza Fazli <eriza.fazli@zii.aero>
2017-02-07 09:14:15 +0100

    Add support for multiple connected monitors

--- brightness.py
+++ brightness.py
@@ -14,7 +14,7 @@ pass_config = click.make_pass_decorator(Config, ensure=True)
 @click.group()
 @pass_config
 def cli(config):
-    config.device = sp.check_output("xrandr --verbose | grep -w connected | cut -d' ' -f1", shell=True).decode().strip()
+    config.device = sp.check_output("xrandr --verbose | grep -w connected | cut -d' ' -f1", shell=True).decode().strip().split('\n')
     config.brightness = float(sp.check_output("xrandr --verbose | awk '/Brightness/ { print $2; exit }'", shell=True))
 
 
@@ -23,7 +23,8 @@ def cli(config):
 def inc(config):
     'Increment brightness by 10%'
     if config.brightness < 1:
-        sp.check_call('xrandr --output {} --brightness {}'.format(config.device, config.brightness + .1), shell=True)
+        for dev in config.device:
+            sp.check_call('xrandr --output {} --brightness {}'.format(dev, config.brightness + .1), shell=True)
 
 
 @cli.command()
@@ -31,4 +32,5 @@ def inc(config):
 def dec(config):
     'Decrement brightness by 10%'
     if config.brightness > 0:
-        sp.check_call('xrandr --output {} --brightness {}'.format(config.device, config.brightness - .1), shell=True)
+        for dev in config.device:
+            sp.check_call('xrandr --output {} --brightness {}'.format(dev, config.brightness - .1), shell=True)
